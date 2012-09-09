3e90526e559a5d2c65a9e2c04916f7235c04b540
枫叶饭团 <maplebeats@gmail.com>
2012-09-10 00:28:37 +0800

    增加IDM导出，似乎工作不正常。。。版本升至0.5

--- xf.user.js
+++ xf.user.js
@@ -3,7 +3,7 @@
 // @namespace   web QQ旋风/xuanfeng
 // @description 选中要下载文件，点击“旋风高速下载”,使用aria2 -i
 // @include     http://lixian.qq.com/main.html*
-// @version     0.4
+// @version     0.5
 // @Author: maplebeats
 // @mail: maplebeats@gmail.com
 // ==/UserScript==
@@ -12,7 +12,7 @@ EF = {};
 var fuck_tx = [];
 EF.test = function()
 {
-    alert("test success")
+    alert("test success");
 }
 
 EF.get_url = function(code)
@@ -27,7 +27,7 @@ EF.get_url = function(code)
             async: false, //TvT
             success: function(data){
                 if(data&&data.ret==0){
-                    fuck_tx.push(data)
+                    fuck_tx.push(data);
                 }
                 else{
                     XF.widget.msgbox.show("请求url失败，FUCK YOU 腾迅",2,2000);
@@ -48,14 +48,14 @@ EF.update = function(data)
 
 EF.init_pop = function()
 {
-    var html = '<div style="height:300px;">'
-    html += '<p>复制命令或者另存为文件，aria2c -i使用文件</p>'
-    html += '<select id="choose"><option value=1>aria2文件</option><option value=2>aria2命令</option><option value=3>wget命令</option></select>'
-    html += '<a id="save-as" style="float:right;" href="data:text/html;charset=utf-8,'+encodeURIComponent(EF.create_data('1'))+'" target="_blank" title="右键另存为" download="test">导出文件(另存为)</a>'
+    var html = '<div style="height:300px;">';
+    html += '<p>复制命令或者另存为文件，aria2c -i file;可以把下载命令共享给其它人的哦。</p>';
+    html += '<select id="choose"><option value=1>aria2文件</option><option value=2>aria2命令</option><option value=3>wget命令</option><option value=4>IDM文件</option></select>';
+    html += '<a id="save-as" style="float:right;" href="data:text/html;charset=utf-8,'+encodeURIComponent(EF.create_data('1'))+'" target="_blank" title="右键另存为" download="test">导出文件(另存为)</a>';
     html += '<textarea id="dl-data" onclick=this.select() style="background:rgba(0,0,0,0);font-size:100%;height:85%;width:100%;overflow:auto;">';
     html += EF.create_data('1');
     html += '</textarea>';
-    html += '</div>'
+    html += '</div>';
     jQuery("#choose_files_table").html(html);
     window.choose_download_files=new xfDialog("choose_download_files");
     XF.widget.msgbox.hide();
@@ -71,22 +71,24 @@ EF.create_data = function(value)
     var url = fuck_tx;
     var html = '';
     for(i in url){
-        var data = url[i]['data']
+        var data = url[i]['data'];
         var cookie = data.com_cookie;
         var http = data.com_url;
         switch(value){
             case '1':
                 html += http+"\n  header=Cookie: FTN5K="+cookie+"\n";
                 html += "  continue=true\n";
-                html += "  max-conection-per-server=5\n"
-                html += "  split=10\n"
-                html += "  parameterized-uri=true\n\n"
+                html += "  max-conection-per-server=5\n";
+                html += "  split=10\n";
+                html += "  parameterized-uri=true\n\n";
                 break;
             case '2':
                 html += "aria2c -c -s10 -x10 --header 'Cookie: FTN5K="+cookie+"' "+http+"\n";
                 break;
             case '3':
                 html += "wget -c --header Cookie:FTN5K="+cookie+" "+http+"\n";
+            case '4':
+                html +='<\r\n'+http+'\r\ncookie: FTN5K='+cookie+'\r\n>\r\n'
             defalut:
                 break;
         }
@@ -96,12 +98,12 @@ EF.create_data = function(value)
 
 EF.handle_arry = function(data)
 {
-    fuck_tx = []
+    fuck_tx = [];
     XF.widget.msgbox.show("正在请求下载连接...",0,20000,true);
     for(i in data){
-        EF.get_url(data[i])
+        EF.get_url(data[i]);
     }
-    EF.init_pop()
+    EF.init_pop();
 }
 
 EventHandler.task_batch2local = function(e)
