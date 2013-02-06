82ce12ae931137641c5adbeb67b7cc9f638918cf
枫叶饭团 <maplebeats@gmail.com>
2013-02-06 17:48:41 +0800

    dos wrap

--- xf.user.js
+++ xf.user.js
@@ -202,7 +202,7 @@ contentEval(function () {
                     html += "wget -c -O '" + name + "' --header Cookie:FTN5K=" + cookie + " " + http + "\n";
                     break;
                 case '5':
-                    html += '<\n' + http + '\n' + 'cookie: FTN5K=' + cookie +'\n>\n\n';
+                    html += '<\r\n' + http + '\r\n' + 'cookie: FTN5K=' + cookie +'\r\n>\r\n\r\n';
                     break;
                 defalut:
                     break;
